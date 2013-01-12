036da1ea89a2575462e78554bcd9b03c89e5d313
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-01-11 22:42:44 -0700

    votes reference drinks

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
-  belongs_to :drink1
-  belongs_to :drink2
+  belongs_to :drink1, class_name: "drink"
+  belongs_to :drink2, class_name: "drink"
   belongs_to :category
   attr_accessible :answer
 end
