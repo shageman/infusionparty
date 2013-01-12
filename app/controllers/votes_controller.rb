45821594943dbaef5a55d002689a4fdbe9d75491
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-01-12 08:43:37 -0700

    Reduce votes controller to what is necessary

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,84 +1,16 @@
 class VotesController < ApplicationController
-  # GET /votes
-  # GET /votes.json
-  def index
-    @votes = Vote.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @votes }
-    end
-  end
-
-  # GET /votes/1
-  # GET /votes/1.json
-  def show
-    @vote = Vote.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @vote }
-    end
-  end
-
-  # GET /votes/new
-  # GET /votes/new.json
   def new
     drinks = Drink.all.sample(2)
     @vote = Vote.new(drink1: drinks.first, drink2: drinks.second)
-
-    respond_to do |format|
-      format.html # new.html.haml
-      format.json { render json: @vote }
-    end
   end
 
-  # GET /votes/1/edit
-  def edit
-    @vote = Vote.find(params[:id])
-  end
-
-  # POST /votes
-  # POST /votes.json
   def create
     @vote = Vote.new(params[:vote])
 
-    respond_to do |format|
-      if @vote.save
-        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
-        format.json { render json: @vote, status: :created, location: @vote }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /votes/1
-  # PUT /votes/1.json
-  def update
-    @vote = Vote.find(params[:id])
-
-    respond_to do |format|
-      if @vote.update_attributes(params[:vote])
-        format.html { redirect_to @vote, notice: 'Vote was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /votes/1
-  # DELETE /votes/1.json
-  def destroy
-    @vote = Vote.find(params[:id])
-    @vote.destroy
-
-    respond_to do |format|
-      format.html { redirect_to votes_url }
-      format.json { head :no_content }
+    if @vote.save
+      redirect_to new_vote_url, notice: 'Vote was successfully created.'
+    else
+      render action: "new"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Infusionparty::Application.routes.draw do
   root to: 'votes#new'
   resources :drinks
-  resources :votes
+  resources :votes, only: [:new, :create]
   resources :results, only: [:index]
 end
