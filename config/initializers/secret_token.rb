5b7bbf39b405d9f2d2500adc3574dbe5e1f20290
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-01-11 21:11:33 -0700

    Added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infusionparty::Application.config.secret_token = '6efcebe50da4b633c85e71e7685d8771458116a25cd79c672ac4efe1db7353b174e01526e4628f2c7d317cea6eb3d863f9e96f2157411a95790c45cef7566ffe'
