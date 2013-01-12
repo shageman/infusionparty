03622d587e859a4ff6b2066668a53b692007273f
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-01-12 16:25:33 -0700

    At least drinks and one vote need to be present for a vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,14 @@ class Vote < ActiveRecord::Base
   belongs_to :drink1, class_name: "Drink"
   belongs_to :drink2, class_name: "Drink"
   attr_accessible :drink1, :drink2, :best_presentation, :most_drinkable, :most_adventurous
+
+  validates_presence_of :drink1, :drink2
+
+  validate :at_least_one_opinion_given
+
+  def at_least_one_opinion_given
+    unless best_presentation.present? || most_drinkable.present? || most_adventurous.present?
+      errors[:best_presentation] << 'Please give at least one opinion!'
+    end
+  end
 end
