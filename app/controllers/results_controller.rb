a030ccb4bc9a687613b88d958fb1b1767728e0d0
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-01-12 09:44:13 -0700

    Results output controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,12 @@
 class ResultsController < ApplicationController
   def index
-    @votes = Vote.all
+    res = "drink1 drink2 presentation drinkability adventurousness"
+    res2 = Vote.all.inject(res) do |result, vote|
+      result += "\n"
+      result += "#{vote.drink1.id} #{vote.drink2.id} #{vote.best_presentation} #{vote.most_drinkable} #{vote.most_adventurous}"
+      result
+    end
+
+    render text: res2, content_type: 'text/plain'
   end
 end
