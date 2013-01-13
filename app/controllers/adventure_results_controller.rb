962c75cfd498900c80fe0673f92e70f1007e2d30
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-01-12 22:14:40 -0700

    big results

--- app/controllers/adventure_results_controller.rb
+++ app/controllers/adventure_results_controller.rb
@@ -3,4 +3,15 @@ class AdventureResultsController < ApplicationController
     AdventureComparison.new.recalculate_all_skills
     @drinks = Drink.order("adventure_mean DESC").all
   end
+
+  def big_results
+    DrinkableComparison.new.recalculate_all_skills
+    @drinkable = Drink.order("drinkable_mean DESC").all
+
+    DrinkableComparison.new.recalculate_all_skills
+    @adventure = Drink.order("adventure_mean DESC").all
+
+    DrinkableComparison.new.recalculate_all_skills
+    @presentable = Drink.order("presentation_mean DESC").all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Infusionparty::Application.routes.draw do
   root to: 'votes#new'
   resources :drinks
+  match 'big_results', to: 'adventure_results#big_results'
   resources :votes, only: [:new, :create]
   resources :presentability_results, only: [:index]
   resources :drinkability_results, only: [:index]
